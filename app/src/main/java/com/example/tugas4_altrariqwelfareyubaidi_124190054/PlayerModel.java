0c77c7df0b688cd7fd5e2f899a0194e9e21ea162
altrayubaidi <72071126+altrayubaidi@users.noreply.github.com>
2021-04-27 14:19:49 +0700

    uploaded

---
+++ app/src/main/java/com/example/tugas4_altrariqwelfareyubaidi_124190054/PlayerModel.java
@@ -0,0 +1,22 @@
+package com.example.tugas4_altrariqwelfareyubaidi_124190054;
+
+public class PlayerModel {
+    private String namaItem;
+    private int gambarItem;
+
+    public String getNamaItem() {
+        return namaItem;
+    }
+
+    public void setNamaItem(String namaItem) {
+        this.namaItem = namaItem;
+    }
+
+    public int getGambarItem() {
+        return gambarItem;
+    }
+
+    public void setGambarItem(int gambarItem) {
+        this.gambarItem = gambarItem;
+    }
+}
